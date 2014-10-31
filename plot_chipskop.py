867bbf5ba7e3f0f20bd5b42600d9cf8264c686de
ASukhanov <sukhanov@bnl.gov>
2014-10-30 22:21:54 -0400

    modified:   plot_chipskop.py

--- plot_chipskop.py
+++ plot_chipskop.py
@@ -11,8 +11,8 @@ import datetime
 ofname = '/tmp/' + datetime.datetime.today().strftime("csk_%y%m%d%H%M.wpl")
 
 # uncomment the following line and comment next line to plot locally
-export_command = '' 
-#export_command = 'scp '+ofname+' andrey@130.199.23.189:/tmp'
+#export_command = '' 
+export_command = 'scp '+ofname+' andrey@130.199.23.189:/tmp'
 
 import sys
 from matplotlib.widgets import Cursor
