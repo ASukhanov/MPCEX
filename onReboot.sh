ee860b3ac2ae13c2a43c55537131c879f016fb82
ASukhanov <sukhanov@bnl.gov>
2014-12-12 10:19:32 -0500

    FEM IDs in onReboot.sh

--- onReboot.sh
+++ onReboot.sh
@@ -9,3 +9,13 @@ gpio -g write 8 0
 gpio export 7 out
 gpio -g mode 7 out
 gpio -g write 7 0
+
+
+# Assing FEM IDs, they should be different for all FEMs at PHENIX
+
+# assign FEM ID for FEMa
+Play_stapl.py i23 10
+
+# assign FEM ID for FEMb
+Play_stapl.py i23 11 -g
+
