940d5372e612d8d0458ff9c6dc0e36227ee3cd70
ASukhanov <sukhanov@bnl.gov>
2015-02-01 14:34:06 -0500

    Added cbfake, femfake, sclk arguments

--- femon.sh
+++ femon.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 # Activate FEMs
 
-# FEMTest mode, internal clock
-CSR10=50101F04
+# Standard mode
+CSR10=16#50000F08
 
-# FEMTest mode, beam clock
-#CSR10=50101F0C
+for arg in "$@"
+do
+  case "$arg" in
+  femfake)	# fake data from FEM
+    CSR10=$((CSR10|0x4))
+    ;;
+  sclk)		# internal clock
+    CSR10=$((CSR10&~0x00000008))
+    ;;
+  cbfake)	# fake data from carrier boards
+    CSR10=$((CSR10|0x00100000))
+    ;;
+  gray)		# gray code 
+    CSR10=$((CSR10|0x00001000))
+    ;;
+  esac
+done
+
+HEXNUM=`printf "%08x\n" $((CSR10))`
+echo Executing: Play_stapl.py i10 $HEXNUM i16 aff00000
 
 # Setup FEM and start sequencer on FEMa
-Play_stapl.py i10 $CSR10 i16 aff00000 > /dev/null
+Play_stapl.py i10 $HEXNUM i16 aff00000 > /dev/null
 
 # and on FEMb
-Play_stapl.py i10 $CSR10 i16 aff00000 -g > /dev/null
+Play_stapl.py i10 $HEXNUM i16 aff00000 -g > /dev/null
