a218ec20536dbc6e98753728453b89b377a3b42c
ASukhanov <sukhanov@bnl.gov>
2015-02-08 17:48:08 -0500

    new file:   test_sdo.sh

--- set_carriers_mode_cn.sh
+++ set_carriers_mode_cn.sh
@@ -6,7 +6,7 @@ USAGE="usage: $0 [a/b]"
 #if [ "$#" -lt "1" ]; then echo $USAGE; exit; fi
 case "$1" in
   "b")
-    echo "Setting carriers on FEMa"
+    echo "Setting carriers on FEMb"
     Play_stapl.py -g i1c 1 > /dev/null
     Play_stapl.py -g -c i30 10140 140 > /dev/null
     Play_stapl.py -g i1c 2 > /dev/null
@@ -17,7 +17,7 @@ case "$1" in
     Play_stapl.py -g -c i30 10140 140 > /dev/null
     ;;
   "a")
-    echo "Setting carriers on FEMb"
+    echo "Setting carriers on FEMa"
     Play_stapl.py i1c 1 > /dev/null
     Play_stapl.py -c i30 10140 140 > /dev/null
     Play_stapl.py i1c 2 > /dev/null
---
+++ test_sdo.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Test sdo serial commands on carrier boards
+
+USAGE="usage: $0 a/b 10/20/40/80 [bclk/sclk]"
+if [ "$#" -lt "3" ]; then echo $USAGE; exit; fi
+
+SP_OPTION=""
+case "$1" in
+  "b")
+    SP_OPTION="-g"
+    ;;
+  "a")
+     SP_OPTION=""
+    ;;
+  *)
+    echo $USAGE
+    exit
+esac
+
+CSR10=16#10000FC8
+  case "$3" in
+  bclk)
+    CSR10=$((CSR10|0x00000008))
+    ;;
+  sclk)         # internal clock
+    CSR10=$((CSR10&~0x00000008))
+    ;;
+  esac
+HEXNUM=`printf "%08x\n" $((CSR10))`
+
+#echo "Executing: Play_stapl.py $SP_OPTION i10 $HEXNUM i16 1ff00000 000000$1 001200$1 00200000 1ff00000 aff00000"
+echo "sequencer:"
+Play_stapl.py $SP_OPTION i10 $HEXNUM i16 1ff00000 000000$1 001200$1 00200000 1ff00000 aff00000 |./splayer_dump.py
+echo "carrier board:"
+Play_stapl.py $SP_OPTION -c i32 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 |./splayer_dump.py
+
