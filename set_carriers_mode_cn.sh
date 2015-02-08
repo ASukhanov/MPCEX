f360698d66355bbb004486ae67648feb938c1c1c
ASukhanov <sukhanov@bnl.gov>
2015-02-08 11:49:39 -0500

    modified:   set_carriers_mode_cn.sh

--- set_carriers_mode_cn.sh
+++ set_carriers_mode_cn.sh
@@ -6,41 +6,44 @@ USAGE="usage: $0 [a/b]"
 #if [ "$#" -lt "1" ]; then echo $USAGE; exit; fi
 case "$1" in
   "b")
-    Play_stapl.py -g i1c 1; > /dev/null
-    Play_stapl.py -g -c i30 10140 140;
-    Play_stapl.py -g i1c 2; > /dev/null
-    Play_stapl.py -g -c i30 10140 140;
-    Play_stapl.py -g i1c 4; > /dev/null
-    Play_stapl.py -g -c i30 10140 140;
-    Play_stapl.py -g i1c 8; > /dev/null
-    Play_stapl.py -g -c i30 10140 140;
+    echo "Setting carriers on FEMa"
+    Play_stapl.py -g i1c 1 > /dev/null
+    Play_stapl.py -g -c i30 10140 140 > /dev/null
+    Play_stapl.py -g i1c 2 > /dev/null
+    Play_stapl.py -g -c i30 10140 140 > /dev/null
+    Play_stapl.py -g i1c 4 > /dev/null
+    Play_stapl.py -g -c i30 10140 140 > /dev/null
+    Play_stapl.py -g i1c 8 > /dev/null
+    Play_stapl.py -g -c i30 10140 140 > /dev/null
     ;;
   "a")
-    Play_stapl.py i1c 1; > /dev/null                                             
-    Play_stapl.py -c i30 10140 140; 
-    Play_stapl.py i1c 2; > /dev/null
-    Play_stapl.py -c i30 10140 140;  
-    Play_stapl.py i1c 4; > /dev/null 
-    Play_stapl.py -c i30 10140 140;  
-    Play_stapl.py i1c 8; > /dev/null
-    Play_stapl.py -c i30 10140 140;
+    echo "Setting carriers on FEMb"
+    Play_stapl.py i1c 1 > /dev/null
+    Play_stapl.py -c i30 10140 140 > /dev/null
+    Play_stapl.py i1c 2 > /dev/null
+    Play_stapl.py -c i30 10140 140 > /dev/null
+    Play_stapl.py i1c 4 > /dev/null
+    Play_stapl.py -c i30 10140 140 > /dev/null
+    Play_stapl.py i1c 8 > /dev/null
+    Play_stapl.py -c i30 10140 140 > /dev/null
     ;;
   *)
-    Play_stapl.py i1c 1; > /dev/null
-    Play_stapl.py -c i30 10140 140;
-    Play_stapl.py i1c 2; > /dev/null
-    Play_stapl.py -c i30 10140 140;
-    Play_stapl.py i1c 4; > /dev/null
-    Play_stapl.py -c i30 10140 140;
-    Play_stapl.py i1c 8; > /dev/null
-    Play_stapl.py -c i30 10140 140;
-    Play_stapl.py -g i1c 1; > /dev/null
-    Play_stapl.py -g -c i30 10140 140;
-    Play_stapl.py -g i1c 2; > /dev/null
-    Play_stapl.py -g -c i30 10140 140;
-    Play_stapl.py -g i1c 4; > /dev/null
-    Play_stapl.py -g -c i30 10140 140;
-    Play_stapl.py -g i1c 8; > /dev/null
-    Play_stapl.py -g -c i30 10140 140;
+    echo "Setting carriers on FEMa and FEMb"
+    Play_stapl.py i1c 1 > /dev/null
+    Play_stapl.py -c i30 10140 140 > /dev/null
+    Play_stapl.py i1c 2 > /dev/null
+    Play_stapl.py -c i30 10140 140 > /dev/null
+    Play_stapl.py i1c 4 > /dev/null
+    Play_stapl.py -c i30 10140 140 > /dev/null
+    Play_stapl.py i1c 8 > /dev/null
+    Play_stapl.py -c i30 10140 140 > /dev/null
+    Play_stapl.py -g i1c 1 > /dev/null
+    Play_stapl.py -g -c i30 10140 140 > /dev/null
+    Play_stapl.py -g i1c 2 > /dev/null
+    Play_stapl.py -g -c i30 10140 140 > /dev/null
+    Play_stapl.py -g i1c 4 > /dev/null
+    Play_stapl.py -g -c i30 10140 140 > /dev/null
+    Play_stapl.py -g i1c 8 > /dev/null
+    Play_stapl.py -g -c i30 10140 140 > /dev/null
 esac
 
