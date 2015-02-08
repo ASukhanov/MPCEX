0e2266d5c20fdd2a25a37f94d56a8070a60ce0a0
ASukhanov <sukhanov@bnl.gov>
2015-02-08 11:36:48 -0500

    modified:   send_calstrobe_to_carriers.sh

--- send_calstrobe_to_carriers.sh
+++ send_calstrobe_to_carriers.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
-SP_OPTION=""
+# Send Calstrobe to carrier boards to copy the configuration into a SEU-safe shadow register
 USAGE="usage: $0 [a/b]"
-if [ "$#" -lt "1" ]; then echo $USAGE; exit; fi
+
+CMD="i16 1ff00000 40 00120000 00200000 1ff00000 4ff00000"
+echo "Executing Play_stapl.py $CMD"
+#if [ "$#" -lt "1" ]; then echo $USAGE; exit; fi
 case "$1" in
   "b")
-    SP_OPTION="-g"
+    Play_stapl.py -g $CMD 
     ;;
   "a")
-     SP_OPTION=""
+    Play_stapl.py $CMD 
     ;;
   *)
-    echo $USAGE
+    Play_stapl.py $CMD 
+    Play_stapl.py -g $CMD 
 esac
-echo "Executing: Play_stapl.py $SP_OPTION i16 1ff00000 40 00120000 00200000 1ff00000 4ff00000"
-Play_stapl.py $SP_OPTION i16 1ff00000 40 00120000 00200000 1ff00000 4ff00000 > /dev/null
