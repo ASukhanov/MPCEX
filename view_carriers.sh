29ebc8dfea8601e865e17ddc2ad9783d3193ff52
ASukhanov <sukhanov@bnl.gov>
2015-02-10 09:51:22 -0500

    modified:   view_carriers.sh

--- view_carriers.sh
+++ view_carriers.sh
@@ -24,6 +24,7 @@ case "$1" in
     Play_stapl.py i1c 4 > /dev/null; # carrier a2
     Play_stapl.py -c i32 0 i33 0;
     Play_stapl.py i1c 8 > /dev/null; # carrier a3
+    Play_stapl.py -c i32 0 i33 0;
     ;;
   *)
     Play_stapl.py -g i1c 1 > /dev/null; # carrier a0
@@ -41,4 +42,5 @@ case "$1" in
     Play_stapl.py i1c 4 > /dev/null; # carrier a2
     Play_stapl.py -c i32 0 i33 0;
     Play_stapl.py i1c 8 > /dev/null; # carrier a3
+    Play_stapl.py -c i32 0 i33 0;
 esac
