3d220e823c12fde168b2f21240d25f52cc5017a4
ASukhanov <sukhanov@bnl.gov>
2015-02-17 09:31:12 -0500

    modified:   splayer_dump.py

--- splayer_dump.py
+++ splayer_dump.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python
-
+# hex dump of the SPlayer output
 import sys
 
 ii=0
 for pline in sys.stdin:
 	try:
-		if ii%8 == 0:
-			print(hex(ii).zfill(6) + ': '),
         	line = pline.split('HEX ')[1][:8]
+		if(len(line)!=8):
+			continue
+                if ii%8 == 0:
+                        print(hex(ii).zfill(6) + ': '),
 		print(line),
 		ii += 1
 		if ii%8 == 0:
