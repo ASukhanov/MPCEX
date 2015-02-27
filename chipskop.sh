cd9e11a90a393ef87445966d295289e282af781f
ASukhanov <sukhanov@bnl.gov>
2015-02-27 18:54:59 -0500

    modified:   chipskop.sh

--- chipskop.sh
+++ chipskop.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 # Control of the FEM on-board logic analyzer
+USAGE="usage: $0 [a/b] arm_l1/arm_seq/arm_gap/plot [trig_offset=0:F] "
+
 TRIG_OFFSET=0
 SP_OPTION=""
-USAGE="usage: $0 [a/b] arm_l1/arm_seq/plot [trig_offset=0:F] "
+CSK_START=1
+CSK_SELECT_CARB0_D=2
+CSK_SELECT_GTM=4
+
+CSK_IDLE=0
+CSK_RUN=$(($CSK_START+$CSK_IDLE))
+#echo $CSK_RUN
 
 if [ "$#" -ge "3" ]; then TRIG_OFFSET=$3; fi
 
@@ -21,19 +29,23 @@ esac
 
 case "$2" in
   "arm_l1")
-     CMD="Play_stapl.py $SP_OPTION i1e 1$TRIG_OFFSET 0$TRIG_OFFSET" # |./splayer_dump.py"
+     ARM=0
     ;;
   "arm_seq")
-     CMD="Play_stapl.py $SP_OPTION i1e 11$TRIG_OFFSET 10$TRIG_OFFSET" # |./splayer_dump.py"
-    ;;  
+     ARM=1
+    ;;
+  "arm_gap")
+     ARM=2
+    ;;
   "plot")
-     CMD="./plot_chipskop.py"
+     ./plot_chipskop.py $1
+     exit
     ;;
   *)
     echo $USAGE
     exit
 esac
-
+CMD="Play_stapl.py $SP_OPTION i1e $ARM$CSK_RUN$TRIG_OFFSET $ARM$CSK_IDLE$TRIG_OFFSET" # |./splayer_dump.py"
 echo "Executing: $CMD"
 $CMD
 # > /dev/null
