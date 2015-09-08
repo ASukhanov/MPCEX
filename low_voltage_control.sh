fa45997c99a6b016fe482361a661912f670a6533
ASukhanov <sukhanov@bnl.gov>
2015-09-08 11:38:01 -0400

    modified:   low_voltage_control.sh

--- low_voltage_control.sh
+++ low_voltage_control.sh
@@ -2,25 +2,38 @@
 usage()
 {
 cat << EOF
-usage: $0 t/b MASK
+usage: $0 MASK
 
 Switch on/off the low voltage channels
 
 OPTIONS:
+  -t   top carrier board (ADDR=3)
+  -b   bottom carrier board (ADDR=2)
+  -uN  usb port
+  -mNN hex mask of enabled channels
 EOF
 }
-case "${1:0:1}" in
-  "b")
-    PREFIX="3"; USB="0";;
-  "t")
-    PREFIX="2"; USB="1";;
-  *)
-    usage; exit 1;
+#defaults
+ADDR="3" # address of bottom carrier
+USB="0"
+MASK="00" # off all the channels
+
+while getopts "tbhu:m:" opt; do
+  case $opt in
+    t) ADDR="2";;
+    b) ADDR="3";;
+    h) usage; exit 1;;
+    u) USB=$OPTARG;;
+    m) MASK=$OPTARG;;
+    \?) echo "Invalid option: -$OPTARG" >&2; exit 1;;
+    :)  echo "Option -$OPTARG requires an argument." >&2; exit 1
 esac
+done
 
-MASK=$2
+CMD="echo -en \"#${ADDR}0S00000${MASK}\x0d\" > /dev/ttyUSB$USB"
 
-CMD="ssh -i ~/.ssh/id_rsa_nopass phnxmpcex@192.168.100.233 'echo -en \"#${PREFIX}0S00000${MASK}\x0d\" > /dev/ttyUSB$USB'"
+# for remote access, uncomment the following line:
+#CMD="ssh -i ~/.ssh/id_rsa_nopass phnxmpcex@192.168.100.233 '$CMD'"
 
 echo "executing: $CMD"
 eval $CMD
