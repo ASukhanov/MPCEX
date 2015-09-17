98bc2aeed1f5c6585af332418ea85ffe2176f797
ASukhanov <sukhanov@bnl.gov>
2015-09-17 17:11:34 -0400

    modified:   dqcapture.py	better reporting

--- dqcapture.py
+++ dqcapture.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python
 
-#usage: ./dump_udp_broadcast [-w] [-d[]
+# usage: ./dqcapture.sh options
+# Data recorder of the gigabit ethernet sream from MPCEX FEM
+#
+# OPTIONS:
+#  -d	dump events
+#  -w	write events to disk
+#  -c	print cell IDs
 
 data_directory = "/tmp/"
 port = 1792  # where do you expect to get a msg?
@@ -26,6 +32,16 @@ dump_trailer = True
 cellids = False # decode cell IDs
 nerrlen=0
 prevlen=0
+errcnt=0
+prev_errcnt=0
+prev_cellid = 0
+lock_count = 0
+MAX_LOCK_COUNT = 5
+expected_length = 0
+delta_time=10
+missed_ev = 0
+missed_events = 0
+prevnum = 0
 
 # hex dump
 #FILTER=''.join([(len(repr(chr(x)))==3) and chr(x) or '.' for x in range(256)])
@@ -51,12 +67,18 @@ def lParity(src):
 		wrd = struct.unpack_from('>H',src[:2])
 		src = src[2:]
 		lpar ^= wrd[0]
-		#if ii:
-		#	print(hex(lpar))
-		#	ii -=1	
 	return lpar
-quiet = 0
-#print(str(sys.argv))
+
+def report():
+                txtout  = str(difftime)+'s, '
+                txtout += hex(evnum)+' ev#'+str(events)+', evLen='+str(msglen)
+		txtout += ' accepted: '+str((events-prevev)/delta_time)+'ev/s,'
+		txtout += ' produced: '+str((events-prevev+missed_events)/delta_time)+'ev/s,'
+                txtout += ' nErrLen='+str(nerrlen)+', nCIDErr='+str(errcnt)
+                txtout += ', bytes in: '+str(bytesin) + ', out: '+str(bytesout)+'kB '
+                print(txtout)
+
+quiet = False
 for opt in sys.argv:
 	if opt == '-w':
 		writing_enabled = True
@@ -65,7 +87,7 @@ for opt in sys.argv:
 		dump_enabled = True
 		print('Dump enabled')
 	if opt == '-q':
-		quiet = 1
+		quiet = True
         if opt == '-c':
                 print('Cell ID Decoding enabled')
 		cellids = True
@@ -94,6 +116,7 @@ while True:
 			elapsed_sec = time.time() - start
 			print('Run stopped, evLen= ' + str(msglen))
 			run_started = 0
+			report()
 			if sfil and not sfil.closed:
 				fpos = sfil.tell()
 				txtout = 'Closed '+sfil.name+'['+str(fpos)+'] after '+str(round(elapsed_sec,1))+'s'
@@ -101,6 +124,8 @@ while True:
 				sfil.close()
 		continue
 	elif run_started == 0:
+		#if expected_length == 0:
+		expected_length = msglen
 		print('Run started, evLen= ' + str(msglen))
 		run_started = 1
 		bytesin = 0.
@@ -117,14 +142,50 @@ while True:
 	if msglen < 100:
 		print('ERROR short event '+str(msglen))
 		continue
-	if quiet == 0:
-                if print_cell_numbers:
-			for ii in range(12):
-				print(struct.unpack_from('>HH',msg(10+258*ii)
+        evnum = struct.unpack_from('>HH',msg)[0]
+	missed_ev = evnum - prevnum
+	prevnum = evnum
+	if missed_ev < 0:
+		missed_ev += 65536 # correct for 16-bit overflow
+	missed_events += missed_ev
+	if msglen != expected_length:
+		nerrlen += 1
+		if not quiet:
+			print('ERROR in ev#'+str(evnum)+'. Unexpected length '+str(msglen)+' != '+str(expected_length))
+		if cellids:
 			continue
+        if cellids:
+		if not quiet:
+			print('ev'+hex(evnum).split('0x')[1].zfill(4)+':'),
+		for ii in range(12):
+                	wrd = struct.unpack_from('>HHHH',msg,2*(10+ii*258))
+			for jj in range(2):
+				if not quiet:
+					print(hex(wrd[jj]).split('0x')[1].zfill(4)),
+			if wrd[jj] != wrd[00]:
+				errcnt +=1
+		if errcnt != prev_errcnt and not quiet:
+			print('_ERR#'+str(errcnt)),
+		prev_errcnt = errcnt
+
+		# lockup detection
+		if wrd[0] == prev_cellid:
+			lock_count += 1;
+		else:
+			lock_count = 0;
+		if lock_count > MAX_LOCK_COUNT:
+			print('\nCell Lockup at event '+hex(evnum-MAX_LOCK_COUNT)+' file closed')
+			if sfil and not sfil.closed:
+				sfil.close() 
+			exit(1)
+		prev_cellid = wrd[00]
+		if not quiet:
+			print('\n'),
+	if not cellids and not quiet:
 		ev_hdr = struct.unpack_from('>HHHHHHHHHHHHHHHHHHHHHH',msg,ev_offset*2)
 		for ii in range(22):
 			print(hex(ev_hdr[ii]).split('0x')[1].zfill(4)),
+		evnum = ev_hdr[0]
 		ev_trl = struct.unpack_from('>HHHH',msg[-8:])
 		print('...'),
 		for ii in range(4):
@@ -134,14 +195,15 @@ while True:
 	if dump_enabled:
 		print(dump(msg))
 	if sfil and not sfil.closed:
+		sfil.write(struct.pack('1i',msglen))
 		sfil.write(msg)
 	difftime = int((time.time() - start))
 	if sfil and not sfil.closed:
 		bytesout = round(sfil.tell()/1000.,1) 
 	else:
 		bytesout = 0.
-	if difftime/10 != olddifftime/10:
-		txtout = str(difftime)+'s: '+str((events-prevev)/10)+'ev/s, evLen='+str(msglen)+' nErrLen='+str(nerrlen)+', bytes in: '+str(bytesin) + ', out: '+str(bytesout)+'kB '
+	if difftime/delta_time != olddifftime/delta_time:
+		report()
 		prevev = events
-		print(txtout)
+		missed_events = 0
 	olddifftime = difftime
