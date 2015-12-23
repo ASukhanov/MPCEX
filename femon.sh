88e6a1023ad6f881189a0805067af0c3df91dffe
ASukhanov <pi@pinut.(none)>
2015-12-23 13:46:55 -0500

    femon.sh internal clock associated with gtm_local

--- femon.sh
+++ femon.sh
@@ -34,25 +34,23 @@ esac
 
 # Working  mode
 CSR10=16#50000F08       # standard
+#CSR10=16#50000F18        # CB1 master
 #CSR10=16#50008F08      # Err_halt, all CB enabled,CB0 master
-#CSR10=16#50008C28       # Err_halt, CB2:3 enabled, CB2-master
-#CSR10=16#50008C38       # Err_halt, CB2:3 enabled, CB3-master
-#CSR10=16#50008C28       # Err_halt, all enabled, CB2-master
 #CSR10=16#50000108      # CB0 enabled, CB0 master
-#CSR10=16#50000528       # CB0 and CB2 enabled, CB2 master.
-#CSR10=16#50000D38      # CB2&3 enabled, CB3 master
-#CSR10=16#50000428      # CB2 enabled
-#CSR10=16#50002D38       # CB2&3 enabled, CB3 master, PARst disabled
+#CSR10=16#50000718       # CB0,CB1CB2 enabled, CB1 master.
 
 #OPTIND=2        # skip first argument
 while getopts "fcgilv:h" opt; do
   #echo "opt=$opt"
   case $opt in
-    f) CSR10=$((CSR10|0x4));        echo "FEMFAke mode on FEM $FEM";;  # FEMFake, fake data from FEM
-    c) CSR10=$((CSR10|0x00100000)); echo "CBFake mode on FEM $FEM";;  # CBFake fake data from carrier boards
-    g) CSR10=$((CSR10|0x00001000)); echo "Gray decoding disabled on FEM $FEM";;  # disable Gray decoding in FPGA, useful for FEMFake and CBFake
-    i) CSR10=$((CSR10&~0x00000008));echo "Internal clock on FEM $FEM";; # internal clock
-    l) CSR10=$((CSR10|0x00040000));echo "GTM switched to local";; 
+    f) CSR10=$((CSR10 | 0x4));        echo "FEMFAke mode on FEM $FEM";;  # FEMFake, fake data from FEM
+    c) CSR10=$((CSR10 | 0x00100000)); echo "CBFake mode on FEM $FEM";;  # CBFake fake data from carrier boards
+    g) CSR10=$((CSR10 | 0x00001000)); echo "Gray decoding disabled on FEM $FEM";;  # disable Gray decoding in FPGA, useful for FEMFake and CBFake
+    i) CSR10=$((CSR10 &~0x00000008)); # internal clock
+       CSR10=$((CSR10 | 0x00010000)); # local GTM
+       echo "Internal clock on FEM $FEM"
+       ;; # internal clock, accompanied by local GTM
+    #l) CSR10=$((CSR10|0x00040000));echo "GTM switched to local";;
     v) VERB=$OPTARG;;
     h) usage; exit;;
     \?) echo "Invalid option: -$OPTARG" >&2; exit 1;;
@@ -70,7 +68,7 @@ HEXNUM=`printf "%08x\n" $((CSR10))`
 #CMD1="Play_stapl.py i16 1ff00000 aff00000 i10 50110fC8 70010fC8"
 
 # The short version below with only sequencer start is sufficient.
-CMD1="Play_stapl.py i16 aff00000 i10"
+CMD1="Play_stapl.py i16 aff00000"
 
 CMD2="Play_stapl.py i10 $HEXNUM"
 
