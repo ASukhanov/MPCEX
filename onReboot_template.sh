a3896b4c4b29500c34fb12fd1993a4d9649b1f14
ASukhanov <sukhanov@bnl.gov>
2016-01-08 12:50:18 -0500

    minor:   onReboot_template.sh

--- onReboot_template.sh
+++ onReboot_template.sh
@@ -12,18 +12,19 @@ gpio -g mode 7 out
 gpio -g write 7 0
 
 # unload/load spi driver, the previous commands affect the spi driver access
-gpio unload spi
-gpio load spi
+#gpio unload spi
+#gpio load spi
 
 #'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 #### local section
 #
-# Assin FEM IDs (default f0 and f1), 
+# Assign FEM IDs (default f0 and f1),
 # they should be changed locally at RPi to be unique.
+# Assume IDs 11..18 for North and 21..23 for South
 
 # Reset FEM and assign FEM ID for FEMa
-Play_stapl.py i10 10000000 0 i23 f0
+Play_stapl.py i10 10000000 0 i23 f0 | ./splayer_dump.py
 
 # The same for FEMb
-Play_stapl.py i10 10000000 0 i23 f1 -g
+Play_stapl.py i10 10000000 0 i23 f1 -g | ./splayer_dump.py
 #,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
