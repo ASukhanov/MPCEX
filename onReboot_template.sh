fadda8d45629d1b71fafebb2a4ce37ddcbf4d05e
ASukhanov <sukhanov@bnl.gov>
2016-01-11 09:31:48 -0500

    onReboot_template.sh rm stapl.stp

--- onReboot_template.sh
+++ onReboot_template.sh
@@ -1,19 +1,24 @@
+#!/bin/bash
 # Configure select line of the RPiLVDS board
 # This file should be modified locally and copied to onReboot.sh
+# version 2016-01-11. rm stapl.stp
+FEMIDA=F1
+FEMIDB=F2
 
 # for JTAG0
-gpio export 8 out
-gpio -g mode 8 out
-gpio -g write 8 0
+/usr/local/bin/gpio export 8 out
+/usr/local/bin/gpio -g mode 8 out
+/usr/local/bin/gpio -g write 8 0
 
 # for JTAG1
-gpio export 7 out
-gpio -g mode 7 out
-gpio -g write 7 0
+/usr/local/bin/gpio export 7 out
+/usr/local/bin/gpio -g mode 7 out
+/usr/local/bin/gpio -g write 7 0
 
 # unload/load spi driver, the previous commands affect the spi driver access
 #gpio unload spi
 #gpio load spi
+echo "GPIO done"
 
 #'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 #### local section
@@ -21,10 +26,14 @@ gpio -g write 7 0
 # Assign FEM IDs (default f0 and f1),
 # they should be changed locally at RPi to be unique.
 # Assume IDs 11..18 for North and 21..23 for South
-
+cd /home/phnxrc/work/MPCEX
 # Reset FEM and assign FEM ID for FEMa
-Play_stapl.py i10 10000000 0 i23 f0 | ./splayer_dump.py
-
+/usr/local/bin/Play_stapl.py i10 10000000 0 i23 $FEMIDA
+# | /home/phnxrc/work/MPCEX/splayer_dump.py
 # The same for FEMb
-Play_stapl.py i10 10000000 0 i23 f1 -g | ./splayer_dump.py
+/usr/local/bin/Play_stapl.py i10 10000000 0 i23 $FEMIDB -g
+# | /home/phnxrc/work/MPCEX/splayer_dump.py
 #,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+echo "FEMIDs set to $FEMIDA $FEMIDB"
+# remove temporary file, created by Splayer
+rm /run/shm/stapl.stp
