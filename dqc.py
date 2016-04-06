8a457eefa9d67f2e4118e5f46c8ffebc4a8ed14b
Andrey <andrey@phast.bnl.gov>
2016-04-06 11:13:41 -0400

    dqc.py

---
+++ dqc.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+'''
+    Simple udp socket server
+'''
+ 
+import socket
+import sys
+import struct
+import binascii
+ 
+HOST = ''   # Symbolic name meaning all available interfaces
+#HOST = '255.255.255.255'
+PORT = 1792 # FEM port
+
+DATSIZE = 7000
+data = bytearray(DATSIZE);
+ 
+# Datagram (udp) socket
+try :
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    print ('Socket created')
+except socket.error, msg :
+    print ('Failed to create socket. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+    sys.exit()
+ 
+# Bind socket to local host and port
+try:
+    s.bind((HOST, PORT))
+except socket.error , msg:
+    print('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+    sys.exit()
+print('Socket bind complete')
+
+#rc =  s.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, bufferSize)
+#s.setblocking(0)
+
+while 1:
+    # receive data
+    datlen, sender = s.recvfrom_into(data, DATSIZE)
+    if not data: 
+        break
+    print('l:'+str(datlen)+' h:'+binascii.hexlify(data[:20])+' d:'+binascii.hexlify(data[20:30])+' t:'+binascii.hexlify(data[datlen-8:datlen]))
+
+s.close()
