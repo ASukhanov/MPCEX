a9ee1b791665951ab6fff8df3db3d0cea63afc21
Andrey <andrey@phast.bnl.gov>
2016-04-06 11:11:11 -0400

    dqcapture.py

--- dqcapture.py
+++ dqcapture.py
@@ -6,10 +6,15 @@
 #  -d	dump events
 #  -w	write events to disk
 #  -c	print cell IDs
+#  -l   limit number of events (to 10000)
+
+# Version 2 2015-12-02 CellId is not checked if zero, useful when only one chain present  
+# Version 3 2015-12-29 -l option
 
 data_directory = "/tmp/"
 port = 1792  # where do you expect to get a msg?
 bufferSize = 7000 # whatever you need
+max_events = 0
 
 import select, socket
 import time
@@ -24,7 +29,6 @@ run_started = -1
 bytesin = 0.
 rc = 0
 events=0
-prevev=0
 writing_enabled = False
 dump_enabled = False
 dump_trailer = True
@@ -39,8 +43,11 @@ MAX_LOCK_COUNT = 10
 expected_length = 0
 delta_time=10
 missed_ev = 0
-missed_events = 0
 prevnum = 0
+nErrL1S = 0
+nErrLPar = 0
+nEmptyEvents = 0
+lEmptyEvent = 28 # length of the empty event
 
 # hex dump
 #FILTER=''.join([(len(repr(chr(x)))==3) and chr(x) or '.' for x in range(256)])
@@ -68,18 +75,23 @@ def lParity(src):
 		lpar ^= wrd[0]
 	return lpar
 
+prevev=0
+missed_events_since_last_report = 0
 def report():
-                txtout  = str(difftime)+'s, '
-                txtout += hex(evnum)+' ev#'+str(events)+', evLen='+str(msglen)
-		txtout += ' accepted: '+str((events-prevev)/delta_time)+'ev/s,'
-		txtout += ' produced: '+str((events-prevev+missed_events)/delta_time)+'ev/s,'
-                txtout += ' nErrLen='+str(nerrlen)+', nCIDErr='
-		if cellids:
-			txtout += str(errcnt)
-		else:
-			txtout += '?'
-                txtout += ', bytes in: '+str(bytesin) + ', out: '+str(bytesout)+'kB '
-                print(txtout)
+  global prevev, missed_events_since_last_report
+  txtout  = str(difftime)+'s, '
+  txtout += hex(evnum)+' ev#'+str(events)+', evLen='+str(msglen)
+  txtout += ' accepted: '+str((events-prevev)/delta_time)+'ev/s,'
+  txtout += ' produced: '+str((events-prevev+missed_events_since_last_report)/delta_time)+'ev/s,'
+  txtout += ' nErrLen='+str(nerrlen)+', nErrL1S='+str(nErrL1S)+', nCIDErr='
+  prevev = events
+  missed_events_since_last_report = 0
+  if cellids:
+          txtout += str(errcnt)
+  else:
+          txtout += '?'
+  txtout += ', bytes in: '+str(bytesin) + ', out: '+str(bytesout)+'kB '
+  print(txtout)
 
 quiet = False
 for opt in sys.argv:
@@ -94,6 +106,8 @@ for opt in sys.argv:
         if opt == '-c':
                 print('Cell ID Decoding enabled')
 		cellids = True
+        if opt == '-l':
+                max_events = 10000
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 #s.bind(('<broadcast>', port))
@@ -106,7 +120,7 @@ while True:
 	msg = result[0][0].recv(bufferSize)
 	events +=1;
 	msglen = len(msg)
-	if msglen != prevlen:
+	if msglen != prevlen and msglen != lEmptyEvent:
 		if prevlen >0:
 			nerrlen +=1;
 	prevlen = msglen
@@ -127,7 +141,6 @@ while True:
 				sfil.close()
 		continue
 	elif run_started == 0:
-		#if expected_length == 0:
 		expected_length = msglen
 		print('Run started, evLen= ' + str(msglen))
 		run_started = 1
@@ -140,33 +153,59 @@ while True:
 				print(txtout)
 		start = time.time()
 		olddifftime = 0
+                nErrL1S = 0 
+                nErrLPar = 0
+                errcnt = 0 
+                nerrlen = 0
+                nEmptyEvents = 0
 
 	# Process event
 	if msglen < 100:
-		print('ERROR short event '+str(msglen))
-		continue
+                if msglen == lEmptyEvent:
+                  nEmptyEvents = nEmptyEvents + 1
+                else:
+		  print('ERROR short event '+str(msglen))
+		  continue
         evnum = struct.unpack_from('>HH',msg)[0]
-	missed_ev = evnum - prevnum
+        if events == 1:
+          prevnum = evnum
+        if prevnum == evnum:
+          missed_ev = 0
+        else:
+          missed_ev = evnum - prevnum - 1
 	prevnum = evnum
 	if missed_ev < 0:
 		missed_ev += 65536 # correct for 16-bit overflow
-	missed_events += missed_ev
+	missed_events_since_last_report += missed_ev
 	if msglen != expected_length:
 		nerrlen += 1
 		if not quiet:
 			print('ERROR in ev#'+str(evnum)+'. Unexpected length '+str(msglen)+' != '+str(expected_length))
 		if cellids:
 			continue
+
+        l1stack=struct.unpack_from('>H',msg,2)
+        #print('l1s[0]='+str(hex(l1stack[0])))
+        l1s = l1stack[0] >> 12
+        #print('l1s='+str(l1s))
+        if l1s == 0 or l1s > 5:
+           nErrL1S = nErrL1S + 1
+
         if cellids:
 		if not quiet:
 			print('ev'+hex(evnum).split('0x')[1].zfill(4)+':'),
 		for ii in range(12):
-                	wrd = struct.unpack_from('>HHHH',msg,2*(10+ii*258))
+                        wrd = struct.unpack_from('>HH',msg,2*(10+ii*258))
+                        if ii == 0:
+				cellid0 = wrd[0]
+				#print('\ncellid='+str(cellid0))
+                        #print('\ncellids='+str(wrd))
 			for jj in range(2):
 				if not quiet:
 					print(hex(wrd[jj]).split('0x')[1].zfill(4)),
-			if wrd[jj] != wrd[00]:
-				errcnt +=1
+				if wrd[jj] != cellid0:
+					if wrd[jj] != 0 or jj == 0: #Vers2
+						errcnt +=1
 		if errcnt != prev_errcnt and not quiet:
 			print('_ERR#'+str(errcnt)),
 		prev_errcnt = errcnt
@@ -188,16 +227,27 @@ while True:
 		if not quiet:
 			print('\n'),
 	if not cellids and not quiet:
-		ev_hdr = struct.unpack_from('>HHHHHHHHHHHHHHHHHHHHHH',msg,ev_offset*2)
-		for ii in range(22):
+                if msglen == lEmptyEvent:
+                  ev_hdr = struct.unpack_from('>HHHHHHHHHH',msg,ev_offset*2)
+                  l=10
+                else:
+                  ev_hdr = struct.unpack_from('>HHHHHHHHHHHHHHHHHHHHHH',msg,ev_offset*2)
+                  l=22
+		for ii in range(l):
 			print(hex(ev_hdr[ii]).split('0x')[1].zfill(4)),
 		evnum = ev_hdr[0]
+                #l1stack = ev_hdr[1]
+                #print('l1s='<<str(l1stack)),
 		ev_trl = struct.unpack_from('>HHHH',msg[-8:])
 		print('...'),
 		for ii in range(4):
 			print(hex(ev_trl[ii]).split('0x')[1].zfill(4)),
 		print('LPar='+hex(lParity(msg))),
-		print('L='+str(msglen))
+		print('L='+str(msglen)),
+                if nErrL1S:
+                  print('_ERRL1S#'+str(nErrL1S)),
+		#print('missed='+str(missed_ev)+','+str(missed_events_since_last_report))
+                print('.')
 	if dump_enabled:
 		print(dump(msg))
 	if sfil and not sfil.closed:
@@ -208,8 +258,13 @@ while True:
 		bytesout = round(sfil.tell()/1000.,1) 
 	else:
 		bytesout = 0.
-	if difftime/delta_time != olddifftime/delta_time:
-		report()
-		prevev = events
-		missed_events = 0
-	olddifftime = difftime
+	#if difftime/delta_time != olddifftime/delta_time:
+        if difftime >= olddifftime + delta_time:
+          report()
+          olddifftime = difftime
+        if max_events > 0:
+          if events >= max_events:
+            print('Stopped after '+str(max_events))
+            if sfil and not sfil.closed:
+              sfil.close()
+            break
