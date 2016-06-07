3324846f528b3ca8d248804d0705f92c7310e760
ASukhanov <sukhanov@bnl.gov>
2016-06-07 14:34:57 -0400

    dialog_run.sh: no need to do cd to working directory

--- dialog_run.sh
+++ dialog_run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # while-menu-dialog: run control of the FEM
+# version v2, 2016-06-07. No need to cd to working directory
 
 # defaults
 FEM="a"
@@ -13,12 +14,10 @@ GEN_PERIOD="15"  # Log2 of generator period, 1: 18KHz, 2: 9KHz
 
 XTRIG="-x -t0"	# enable external trigger and disable internal triggers
 
-CAPLOG="-v"	# extended logging of data capture
+CAPLOG="-v"	# extended logging of the data capturing
 
 BIAS="100"       # Bias level [V]
 BIAS_STANDBY="50" # Bias standby level [V]
-#BIAS="50"	# Bias level [V]
-#BIAS_STANDBY="25" # Bias standby level [V]
 
 #FEMFAKE="-f" # Fake data from FEM for testing the FEM transmission interface
 
@@ -30,6 +29,10 @@ HEIGHT=0
 WIDTH=0
 DAQ="OFF"
 RUN="stopped"
+
+WORKDIR="/home/pi/work/"
+FEMDIR="${WORKDIR}MPCEX/"
+SPIDIR="${WORKDIR}SPI/"
 
 display_result() {
   dialog --title "$1" \
@@ -92,48 +95,48 @@ while true; do
           result="Run is already started"
           display_result "Run is already started";
         else
-          result=$(./femon.sh $FEM $FEMFAKE; ./gtm_lkl.sh $FEM -p$GEN_PERIOD $XTRIG -g$GEN_MODE;)
+          result=$(${FEMDIR}femon.sh $FEM $FEMFAKE; ${FEMDIR}gtm_lkl.sh $FEM -p$GEN_PERIOD $XTRIG -g$GEN_MODE;)
           display_result "Local generator $GEN_MODE started"
           RUN="started"
         fi
       ;;
     o )
-      result=$(./gtm_lkl.sh $FEM -gstop; ./femoff.sh)
+      result=$(${FEMDIR}gtm_lkl.sh $FEM -gstop; ${FEMDIR}femoff.sh)
       RUN="stopped"
       display_result "Run stopped"
       ;;
     p )
       if [ $DAQ == "ON" ];
-        then DAQ="OFF"; ./gtm_lkl.sh $FEM -gstop;
-        else DAQ="ON"; ./gtm_lkl.sh $FEM -p$GEN_PERIOD -g$GEN_MODE;
+        then DAQ="OFF"; ${FEMDIR}gtm_lkl.sh $FEM -gstop;
+        else DAQ="ON"; ${FEMDIR}gtm_lkl.sh $FEM -p$GEN_PERIOD -g$GEN_MODE;
       fi
       #dialog_pause=$(echo "DAQ #DAQ")
       display_result "DAQ $DAQ"
       ;;
     f )
-      result=$(./view_status.sh $FEM)
+      result=$(${FEMDIR}view_status.sh $FEM)
       display_result "FEM status"
       ;;
     c )
-      result=$(./view_carriers.sh $FEM$CAR)
+      result=$(${FEMDIR}view_carriers.sh $FEM$CAR)
       display_result "Carrier status"
       ;;
     d )
-      result=$(./doall.sh $FEM)
+      result=$(cd ${FEMDIR}; ./doall.sh $FEM; cd $OLDPWD)
       display_result "Downloading Carriers of FEM$FEM"
       ;;
     e )
-      result=$(script -q -c "~/work/MPCEX/dqc.py $CAPLOG $TIMESTAMPING" /dev/null > /tmp/dqc.log&)
+      result=$(script -q -c "${FEMDIR}dqc.py $CAPLOG $TIMESTAMPING" /dev/null > /tmp/dqc.log&)
       comment="To watch capture progress: tail -f /tmp/dqc.log"
       display_result "Capture data on ethernet port                    "
       ;;
     ew1 )
-      result=$(script -q -c "~/work/MPCEX/dqc.py -w1 $CAPLOG $TIMESTAMPING" /dev/null > /tmp/dqc.log&)
+      result=$(script -q -c "${FEMDIR}dqc.py -w1 $CAPLOG $TIMESTAMPING" /dev/null > /tmp/dqc.log&)
       comment="To watch capture progress: tail -f /tmp/dqc.log"
       display_result "Capture and write data to file system 1          "
       ;;
     ew2 )
-      result=$(script -q -c "~/work/MPCEX/dqc.py -w2 $CAPLOG $TIMESTAMPING" /dev/null > /tmp/dqc.log&)
+      result=$(script -q -c "${FEMDIR}dqc.py -w2 $CAPLOG $TIMESTAMPING" /dev/null > /tmp/dqc.log&)
       comment="To watch capture progress: tail -f /tmp/dqc.log"
       display_result "Capture and write data to /tmp                   "
       ;;
@@ -142,15 +145,15 @@ while true; do
       display_result "Stop data capture"
       ;;
     b )
-      result=$( ~/work/spi/dadc_set_bias.sh $BIAS)
+      result=$( ${SPIDIR}dadc_set_bias.sh $BIAS)
       display_result "Bias On                       "
       ;;
     bs )
-      result=$( ~/work/spi/dadc_set_bias.sh $BIAS_STANDBY)
+      result=$( ${SPIDIR}dadc_set_bias.sh $BIAS_STANDBY)
       display_result "Bias Standby                  "
       ;;
     bf )
-      result=$( ~/work/spi/dadc_set_bias.sh 0)
+      result=$( ${SPIDIR}dadc_set_bias.sh 0)
       display_result "Bias Off"
       ;;
   esac
